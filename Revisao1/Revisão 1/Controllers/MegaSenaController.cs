7f048659dc161630fa426458589701d8ca7b9c3b
Kaos620 <RafaelCardoso6200@gmail.com>
2023-09-20 20:22:53 -0300

    CommitRevisao

---
+++ Revisao1/Revisão 1/Controllers/MegaSenaController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Revisão_1.Models.Request;
+using Revisão_1.Models.Validations;
+using System.Runtime.CompilerServices;
+using System.Text.Json.Serialization;
+
+namespace Revisão_1.Controllers
+{
+    [ApiController]
+    [Route("Controller")]
+    public class MegaSenaController : ControllerBase
+    {
+        private readonly string _megaSenaCaminhoDoArquivo;
+
+        public MegaSenaController()
+        {
+            _megaSenaCaminhoDoArquivo = Path.Combine(Directory.GetCurrentDirectory(), "Data", "MegaSena.Json");
+        }
+
+        private List<RegistrarJogoViewModel> LerJogosDoArquivo()
+        { 
+            if(!System.IO.File.Exists(_megaSenaCaminhoDoArquivo))
+            {
+                return new List<RegistrarJogoViewModel>();
+            }
+
+            string json = System.IO.File.ReadAllText(_megaSenaCaminhoDoArquivo);
+            return JsonConvert.DeserializeObject<List<RegistrarJogoViewModel>>(json);
+        }
+
+        private void EscreverNoArquivo (List<RegistrarJogoViewModel> jogos) 
+        {
+            string json = JsonConvert.SerializeObject(jogos);
+            System.IO.File.WriteAllText(_megaSenaCaminhoDoArquivo, json);
+        }
+
+        [HttpPost]
+        public IActionResult Post(RegistrarJogoViewModel registrarJogoViewModel)
+        {
+
+            if (registrarJogoViewModel == null)
+            {
+                return BadRequest();
+            }
+
+            var jogosRealizados = LerJogosDoArquivo();
+
+            registrarJogoViewModel.DataJogo = DateTime.Now;
+
+            jogosRealizados.Add(registrarJogoViewModel);
+            
+            EscreverNoArquivo(jogosRealizados);
+            
+            return Ok("Seu cadastro na Mega Sena foi registrado com Sucesso");
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(LerJogosDoArquivo());
+        }
+        
+    }
+}
---
+++ Revisao1/Revisão 1/Models/Request/RegistrarJogoViewModel.cs
@@ -0,0 +1,50 @@
+﻿
+using Revisão_1.Models.Validations;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Revisão_1.Models.Request
+{
+    public class RegistrarJogoViewModel
+    {
+
+        [Required(ErrorMessage = "Digite um Nome")]
+        [MinLength(3, ErrorMessage = "O nome deve ter ao menos 3 letras")]
+        [MaxLength(255, ErrorMessage = "Limite maximo de letras atingido")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Digite um CPF")]
+        [CPFValidation]
+        public string CPF { get; set; }
+
+        [Required(ErrorMessage = "Digite um Numero")]
+        [Range(1,60, ErrorMessage = "Deve ser um numero entre 1 a 60")]
+        public int PrimeiroNro { get; set; }
+
+        [Required(ErrorMessage = "Digite um Numero")]
+        [Range(1, 60, ErrorMessage = "Deve ser um numero entre 1 a 60")]
+        public int SegundoNro { get; set; }
+
+        [Required(ErrorMessage = "Digite um Numero")]
+        [Range(1, 60, ErrorMessage = "Deve ser um numero entre 1 a 60")]
+        public int TerceiroNro { get; set; }
+
+        [Required(ErrorMessage = "Digite um Numero")]
+        [Range(1, 60, ErrorMessage = "Deve ser um numero entre 1 a 60")]
+        public int QuartoNro { get; set; }
+
+        [Required(ErrorMessage = "Digite um Numero")]
+        [Range(1, 60, ErrorMessage = "Deve ser um numero entre 1 a 60")]
+        public int QuintoNro { get; set; }
+
+        [Required(ErrorMessage = "Digite um Numero")]
+        [Range(1, 60, ErrorMessage = "Deve ser um numero entre 1 a 60")]
+        public int SextoNro { get; set; }
+
+        [JsonIgnore]
+        public DateTime DataJogo { get; set; }
+
+        [NumerosValidation(ErrorMessage = "Deve ter 6 numeros diferentes")]
+        public int[] NumeroDoJogo { get; set; }
+    }
+}
